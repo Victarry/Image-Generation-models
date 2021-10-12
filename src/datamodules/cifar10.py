a59055e87f5c6103590f60771ae4b762358f99fd
Zhenhuan Liu <nkulzh16@gmail.com>
2021-10-12 03:17:37 -0400

    Add cifar10 dataset.

---
+++ src/datamodules/cifar10.py
@@ -0,0 +1,53 @@
+from typing import Optional
+import torch
+import pytorch_lightning as pl
+from torch.utils.data import random_split, DataLoader
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
+
+
+class CIFAR10DataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        data_dir: str = "./data",
+        batch_size: int = 64,
+        num_workers: int = 8,
+        **kargs
+    ):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.transform = transforms.Compose([transforms.ToTensor()])
+
+        self.num_classes = 10
+
+    def prepare_data(self):
+        # download
+        CIFAR10(self.data_dir, train=True, download=True)
+        CIFAR10(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+        # Assign train/val datasets for use in dataloaders
+        train_full = CIFAR10(self.data_dir, train=True, transform=self.transform)
+        self.train_data, self.val_data = random_split(train_full, [45000, 5000])
+        self.test_data = CIFAR10(self.data_dir, train=False, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_data,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+        )
+
+    # def val_dataloader(self):
+    #     return DataLoader(
+    #         self.val_data, batch_size=self.batch_size, num_workers=self.num_workers
+    #     )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_data, batch_size=self.batch_size, num_workers=self.num_workers
+        )
--- src/networks/conv32.py
+++ src/networks/conv32.py
@@ -14,25 +14,25 @@ class Decoder(BaseNetwork):
         super().__init__(input_channel, output_channel)
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(input_channel, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(input_channel, ngf * 8, 2, 1, 0, bias=False),
             get_norm_layer(batch_norm)(ngf * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
+            # state size. (ngf*8) x 2 x 2
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             get_norm_layer(batch_norm)(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
+            # state size. (ngf*4) x 4 x 4
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             get_norm_layer(batch_norm)(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
+            # state size. (ngf*2) x 8 x 8 
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             get_norm_layer(batch_norm)(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
+            # state size. (ngf) x 16 x 16
             nn.ConvTranspose2d(ngf, output_channel, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
+            # state size. (nc) x 32 x 32 
         )
 
     def forward(self, input):
@@ -45,23 +45,23 @@ class Encoder(BaseNetwork):
     def __init__(self, input_channel, output_channel, ndf, batch_norm=True):
         super().__init__(input_channel, output_channel)
         self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
+            # input is (nc) x 32 x 32
             nn.Conv2d(input_channel, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
+            # state size. (ndf) x 16 x 16
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             get_norm_layer(batch_norm)(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
+            # state size. (ndf*2) x 8 x 8
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             get_norm_layer(batch_norm)(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
+            # state size. (ndf*4) x 4 x 4
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             get_norm_layer(batch_norm)(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, output_channel, 4, 1, 0, bias=False),
+            # state size. (ndf*8) x 2 x 2
+            nn.Conv2d(ndf * 8, output_channel, 2, 1, 0, bias=False),
         )
 
     def forward(self, input):
